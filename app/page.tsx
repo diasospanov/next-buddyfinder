83d12a0d661999f7894918e3498b0898c1d3774b
Di <dias.ospanov@gmail.com>
2023-02-23 20:56:05 +0200

    Add styling to home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import Link from 'next/link';
 import styles from './page.module.scss';
+
+export const metadata = {
+  title: 'Buddy Finder',
+  description: 'Find your sport companion',
+};
 
 export default function HomePage() {
   return (
-    <main className={styles.main}>
-      <h1>Welcome to BuddyFinder</h1>
-      <div>Running</div>
-      <div>Cycling</div>
+    <main>
+      <h1 className={styles.h1}>Welcome to Buddy Finder</h1>
+      <section className={styles.section}>
+        <Link href="/running">Running</Link>
+        <Link href="/cycling">Cycling</Link>
+      </section>
     </main>
   );
 }
