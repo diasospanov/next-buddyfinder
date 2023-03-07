cac7f2b5b8982c65cf5df3b836292557b3258c40
Di <dias.ospanov@gmail.com>
2023-03-07 14:44:25 +0200

    Create sessions function.

---
+++ database/sessions.ts
@@ -0,0 +1,35 @@
+import { cache } from 'react';
+import { sql } from './connect';
+
+type Session = {
+  id: number;
+  token: string;
+  csrfSecret: string;
+};
+
+export const createSession = cache(
+  async (token: string, userId: number, csrfSecret: string) => {
+    const [session] = await sql<{ id: number; token: string }[]>`
+      INSERT INTO sessions
+        (token, user_id, csrf_secret)
+      VALUES
+        (${token}, ${userId}, ${csrfSecret})
+      RETURNING
+        id,
+        token
+    `;
+
+    await deleteExpiredSessions();
+
+    return session;
+  },
+);
+
+export const deleteExpiredSessions = cache(async () => {
+  await sql`
+    DELETE FROM
+      sessions
+    WHERE
+      expiry_timestamp < now()
+  `;
+});
