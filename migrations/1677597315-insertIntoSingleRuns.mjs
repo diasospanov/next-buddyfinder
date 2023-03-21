cb43aee38ea9c357db8e98de631993c1439d9b94
Di <dias.ospanov@gmail.com>
2023-03-21 10:18:36 +0200

    Add playwright and deploy yml.

--- migrations/1677597315-insertIntoSingleRuns.mjs
+++ migrations/1677597315-insertIntoSingleRuns.mjs
@@ -17,42 +17,6 @@ const singleRuns = [
     distance: 8,
     pace: 7,
   },
-  {
-    id: 3,
-    organiser: 'Runner-1',
-    participant: 'Runner-3',
-    date: '14/04/23',
-    time: '16:00',
-    distance: 15,
-    pace: 5.5,
-  },
-  {
-    id: 4,
-    organiser: 'Runner-1',
-    participant: 'Runner-4',
-    date: '21/04/23',
-    time: '09:00',
-    distance: 10,
-    pace: 6,
-  },
-  {
-    id: 5,
-    organiser: 'Runner-1',
-    participant: 'Runner-5',
-    date: '28/04/23',
-    time: '16:00',
-    distance: 5,
-    pace: 7,
-  },
-  {
-    id: 6,
-    organiser: 'Runner-1',
-    participant: 'Runner-6',
-    date: '01/05/23',
-    time: '16:30',
-    distance: 10,
-    pace: 4.5,
-  },
 ];
 
 export async function up(sql) {
